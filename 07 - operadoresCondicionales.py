63fa3cac6df726dcde247af2628f38b54ee4d5d2
Sergio <chejo.lopez2015.cl@gmail.com>
2020-06-12 20:57:11 -0600

    Holis

---
+++ 07 - operadoresCondicionales.py
@@ -0,0 +1,10 @@
+day = "monday"
+meal = "pizza"
+
+
+isDay = day == "monday"
+
+if day == "monday":
+    print(f"we are eating {meal}")
+elif day != "monday":
+    print("we are eating healthy food")
